b9ee807e2d8cc37da9a00e6a95e24717faf9e68c
Dan Finigan <danielfinigan@gmail.com>
2016-11-03 20:44:30 -0400

    created skeletons of TileGenerator and Tile classes

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    [SerializeField] private float x;
+    [SerializeField] private float y;
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector2(x, y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
