f804805a1e94ecbb838683bd7dd1284b2699af9f
Dan Finigan <danielfinigan@gmail.com>
2016-11-06 16:41:14 -0500

    coded AddTile method and created tile prefabs

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,13 +6,21 @@ public class Tile : MonoBehaviour {
     [SerializeField] private float x;
     [SerializeField] private float y;
 
+    public Tile instance;
+
 	// Use this for initialization
 	void Start () {
         this.transform.position = new Vector2(x, y);
+        instance = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void MergeTiles(Tile toBeDeleted)
+    {
+
+    }
 }
--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TileGenerator : MonoBehaviour {
 
+    private static int gridSize = 4;
+    public Tile[,] grid = new Tile[gridSize, gridSize];
+    public List<Tile> TilePrefabs = new List<Tile>();
+    public TileGenerator instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
 	// Use this for initialization
-	void Start () {
-	
+
+    public void AddTile()
+    {
+        bool isEmpty = true;
+        while(isEmpty)
+        {
+            int gridX = Random.Range(0, gridSize);
+            int gridY = Random.Range(0, gridSize);
+            if(grid[gridX, gridY] == null)
+            {
+                grid[gridX,gridY] = Instantiate(TilePrefabs[0]);
+                isEmpty = false;
+            }
+        }
+    }
+
+    public void RemoveTile()
+    {
+
+    }
+
+    public void Equals(Tile other)
+    {
+
+    }
+
+	void Start ()
+    {
+        AddTile();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
